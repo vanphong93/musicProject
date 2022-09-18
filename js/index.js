1ba871935f172e4dbc39c02a84bdad479087a784
Ho Van Phong <phongckm93@gmail.com>
2022-09-18 20:44:09 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const MUSIC_LIST = [
+    {
+      img: "./images/Faded.jpg",
+      name: "Faded",
+      artist: "Alan Walker",
+      music: "./musics/Fade.mp3",
+    },
+    {
+      img: "./images/Lily.png",
+      name: "Lily",
+      artist: "Alan Walker",
+      music: "./musics/Lily.mp3",
+    },
+    {
+      img: "./images/TheNights.jpg",
+      name: "The Night",
+      artist: "Avicii (Cover by AngieN.)",
+      music: "./musics/TheNights.mp3",
+    },
+    {
+      img: "./images/VuHoiHoaTrang.jpg",
+      name: "假面舞会",
+      artist: "很美味",
+      music: "./musics/VuHoiHoaTrang.mp3",
+    },
+    {
+      img: "./images/VuHoiHoaTrang.jpg",
+      name: "rain",
+      artist: "natural",
+      music: "./musics/gale160.mp3",
+    },
+    {
+      img: "./images/VuHoiHoaTrang.jpg",
+      name: "rain",
+      artist: "natural",
+      music: "./musics/ocean.mp3",
+    },
+  ];
